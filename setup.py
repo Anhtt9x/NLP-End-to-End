74691e671340b00f46acd18daf7db7a762fa107b
Anhtt9x <anhtt454598@gmail.com>
2024-10-21 16:56:59 +0700

    updated

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup , find_packages
 
-setup(name="Reuse_structure",
+setup(name="NLP-End-to-End",
       version="0.0.0",
       author="Anhtt9x",
       author_email="anhtt454598@gmail.com",
